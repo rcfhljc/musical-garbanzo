1dc62111d89084ad9fed3c35fa241af9f3c270bf
rcfhljc <nini2001@inbox.ru>
2023-08-27 18:59:24 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Задача № 10
+# import random
+# n = int(input("Введите количество монеток (n): "))
+# coins = list(range(n)) # создаем список из монет
+# random.shuffle(coins) # перемешиваем монеты
+# flips = 0 # счетчик перевернутых монеток
+# while any(coin == 0 for coin in coins): # пока не все монеты лежат одной стороной
+#  coin = coins.pop(0) # переворачиваем первую монету
+#  flips += 1 # увеличиваем счетчик перевернутых монет
+# print(f"Минимальное количество перевернутых монеток: {flips}")
+
+# Задача 12
+# def find_numbers(s, p):
+#     for x in range(1, 1001):
+#         y = s - x
+#         if x * y == p:
+#             return x, y
+
+# sum_hint = int(input("Введите сумму чисел: "))
+# product_hint = int(input("Введите произведение чисел: "))
+
+# result = find_numbers(sum_hint, product_hint)
+
+# if result:
+#     print(f"Задуманные числа Петей: {result[0]} и {result[1]}")
+# else:
+#     print("Не удалось найти числа, соответствующие подсказкам.")
+
+def powers_of_two(n):
+    powers = []
+    k = 0
+    power = 1
+
+    while power <= n:
+        powers.append(power)
+        k += 1
+        power = 2 ** k
+
+    return powers
+
+number = int(input("Введите число N: "))
+
+result = powers_of_two(number)
+
+print("Целые степени двойки, не превосходящие", number, ":")
+for num in result:
+    print(num)
