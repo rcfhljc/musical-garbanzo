47e2a1ecc21f1f943b75ad4a9e85ad7a1b943266
rcfhljc <nini2001@inbox.ru>
2023-08-27 18:59:36 +1000

    first commit

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
-# Задача № 10
-# import random
-# n = int(input("Введите количество монеток (n): "))
-# coins = list(range(n)) # создаем список из монет
-# random.shuffle(coins) # перемешиваем монеты
-# flips = 0 # счетчик перевернутых монеток
-# while any(coin == 0 for coin in coins): # пока не все монеты лежат одной стороной
-#  coin = coins.pop(0) # переворачиваем первую монету
-#  flips += 1 # увеличиваем счетчик перевернутых монет
-# print(f"Минимальное количество перевернутых монеток: {flips}")
+Задача 10
+import random
+n = int(input("Введите количество монеток (n): "))
+coins = list(range(n)) 
+random.shuffle(coins) 
+flips = 0 
+while any(coin == 0 for coin in coins): # пока не все монеты лежат одной стороной
+ coin = coins.pop(0) 
+ flips += 1 
+print(f"Минимальное количество перевернутых монеток: {flips}")
 
-# Задача 12
-# def find_numbers(s, p):
-#     for x in range(1, 1001):
-#         y = s - x
-#         if x * y == p:
-#             return x, y
+Задача 12
+def find_numbers(s, p):
+    for x in range(1, 1001):
+        y = s - x
+        if x * y == p:
+            return x, y
 
-# sum_hint = int(input("Введите сумму чисел: "))
-# product_hint = int(input("Введите произведение чисел: "))
+sum_hint = int(input("Введите сумму чисел: "))
+product_hint = int(input("Введите произведение чисел: "))
 
-# result = find_numbers(sum_hint, product_hint)
+result = find_numbers(sum_hint, product_hint)
 
-# if result:
-#     print(f"Задуманные числа Петей: {result[0]} и {result[1]}")
-# else:
-#     print("Не удалось найти числа, соответствующие подсказкам.")
+if result:
+    print(f"Задуманные числа Петей: {result[0]} и {result[1]}")
+else:
+    print("Не удалось найти числа, соответствующие подсказкам.")
 
+Задача 14
 def powers_of_two(n):
     powers = []
     k = 0
